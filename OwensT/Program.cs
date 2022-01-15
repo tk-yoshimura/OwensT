d6b2cc475fe15f3195217387990d8cf0abfb7380
tk-yoshimura <yoshimura8192@gmail.com>
2022-01-16 00:36:06 +0900

    Initial commit

---
+++ OwensT/Program.cs
@@ -0,0 +1,108 @@
+﻿using MultiPrecision;
+using System;
+
+namespace OwensT {
+    internal class Program {
+        static void Main() {
+            Check();
+
+            Console.WriteLine("END");
+            Console.Read();
+        }
+
+        static void Check() { 
+            MultiPrecision<Pow2.N4> n4_t1_1 = OwenTPatefieldTandyAlgo<Pow2.N4>.T1(0.0625, 0.25);
+            MultiPrecision<Pow2.N4> n4_t5_1 = OwenTPatefieldTandyAlgo<Pow2.N4>.T5(0.0625, 0.25);
+            MultiPrecision<Pow2.N4> n4_t2_2 = OwenTPatefieldTandyAlgo<Pow2.N4>.T2(6.5, 0.4375);
+            MultiPrecision<Pow2.N4> n4_t5_2 = OwenTPatefieldTandyAlgo<Pow2.N4>.T5(6.5, 0.4375);
+            MultiPrecision<Pow2.N4> n4_t4_3 = OwenTPatefieldTandyAlgo<Pow2.N4>.T4(7, 0.96875);
+            MultiPrecision<Pow2.N4> n4_t5_3 = OwenTPatefieldTandyAlgo<Pow2.N4>.T5(7, 0.96875);
+            MultiPrecision<Pow2.N4> n4_t2_4 = OwenTPatefieldTandyAlgo<Pow2.N4>.T2(4.78125, 0.0625);
+            MultiPrecision<Pow2.N4> n4_t4_4 = OwenTPatefieldTandyAlgo<Pow2.N4>.T4(4.78125, 0.0625);
+            MultiPrecision<Pow2.N4> n4_t5_4 = OwenTPatefieldTandyAlgo<Pow2.N4>.T5(4.78125, 0.0625);
+            MultiPrecision<Pow2.N4> n4_t4_5 = OwenTPatefieldTandyAlgo<Pow2.N4>.T4(2, 0.5);
+            MultiPrecision<Pow2.N4> n4_t5_5 = OwenTPatefieldTandyAlgo<Pow2.N4>.T5(2, 0.5);
+            MultiPrecision<Pow2.N4> n4_t5_6 = OwenTPatefieldTandyAlgo<Pow2.N4>.T5(1, 0.9999975m);
+
+            MultiPrecision<Pow2.N8> n8_t1_1 = OwenTPatefieldTandyAlgo<Pow2.N8>.T1(0.0625, 0.25);
+            MultiPrecision<Pow2.N8> n8_t5_1 = OwenTPatefieldTandyAlgo<Pow2.N8>.T5(0.0625, 0.25);
+            MultiPrecision<Pow2.N8> n8_t2_2 = OwenTPatefieldTandyAlgo<Pow2.N8>.T2(6.5, 0.4375);
+            MultiPrecision<Pow2.N8> n8_t5_2 = OwenTPatefieldTandyAlgo<Pow2.N8>.T5(6.5, 0.4375);
+            MultiPrecision<Pow2.N8> n8_t4_3 = OwenTPatefieldTandyAlgo<Pow2.N8>.T4(7, 0.96875);
+            MultiPrecision<Pow2.N8> n8_t5_3 = OwenTPatefieldTandyAlgo<Pow2.N8>.T5(7, 0.96875);
+            MultiPrecision<Pow2.N8> n8_t2_4 = OwenTPatefieldTandyAlgo<Pow2.N8>.T2(4.78125, 0.0625);
+            MultiPrecision<Pow2.N8> n8_t4_4 = OwenTPatefieldTandyAlgo<Pow2.N8>.T4(4.78125, 0.0625);
+            MultiPrecision<Pow2.N8> n8_t5_4 = OwenTPatefieldTandyAlgo<Pow2.N8>.T5(4.78125, 0.0625);
+            MultiPrecision<Pow2.N8> n8_t4_5 = OwenTPatefieldTandyAlgo<Pow2.N8>.T4(2, 0.5);
+            MultiPrecision<Pow2.N8> n8_t5_5 = OwenTPatefieldTandyAlgo<Pow2.N8>.T5(2, 0.5);
+            MultiPrecision<Pow2.N8> n8_t5_6 = OwenTPatefieldTandyAlgo<Pow2.N8>.T5(1, 0.9999975m);
+
+            MultiPrecision<Pow2.N16> n16_t1_1 = OwenTPatefieldTandyAlgo<Pow2.N16>.T1(0.0625, 0.25);
+            MultiPrecision<Pow2.N16> n16_t5_1 = OwenTPatefieldTandyAlgo<Pow2.N16>.T5(0.0625, 0.25);
+            MultiPrecision<Pow2.N16> n16_t2_2 = OwenTPatefieldTandyAlgo<Pow2.N16>.T2(6.5, 0.4375);
+            MultiPrecision<Pow2.N16> n16_t5_2 = OwenTPatefieldTandyAlgo<Pow2.N16>.T5(6.5, 0.4375);
+            MultiPrecision<Pow2.N16> n16_t4_3 = OwenTPatefieldTandyAlgo<Pow2.N16>.T4(7, 0.96875);
+            MultiPrecision<Pow2.N16> n16_t5_3 = OwenTPatefieldTandyAlgo<Pow2.N16>.T5(7, 0.96875);
+            MultiPrecision<Pow2.N16> n16_t2_4 = OwenTPatefieldTandyAlgo<Pow2.N16>.T2(4.78125, 0.0625);
+            MultiPrecision<Pow2.N16> n16_t4_4 = OwenTPatefieldTandyAlgo<Pow2.N16>.T4(4.78125, 0.0625);
+            MultiPrecision<Pow2.N16> n16_t5_4 = OwenTPatefieldTandyAlgo<Pow2.N16>.T5(4.78125, 0.0625);
+            MultiPrecision<Pow2.N16> n16_t4_5 = OwenTPatefieldTandyAlgo<Pow2.N16>.T4(2, 0.5);
+            MultiPrecision<Pow2.N16> n16_t5_5 = OwenTPatefieldTandyAlgo<Pow2.N16>.T5(2, 0.5);
+            MultiPrecision<Pow2.N16> n16_t5_6 = OwenTPatefieldTandyAlgo<Pow2.N16>.T5(1, 0.9999975m);
+
+            Console.WriteLine("T1(0.0625, 0.25)");
+            Console.WriteLine(n4_t1_1);
+            Console.WriteLine(n8_t1_1);
+            Console.WriteLine(n16_t1_1);
+            Console.WriteLine("T5(0.0625, 0.25)");
+            Console.WriteLine(n4_t5_1);
+            Console.WriteLine(n8_t5_1);
+            Console.WriteLine(n16_t5_1);
+                        
+            Console.WriteLine("T2(6.5, 0.4375)");
+            Console.WriteLine(n4_t2_2);
+            Console.WriteLine(n8_t2_2);
+            Console.WriteLine(n16_t2_2);
+            Console.WriteLine("T5(6.5, 0.4375)");
+            Console.WriteLine(n4_t5_2);
+            Console.WriteLine(n8_t5_2);
+            Console.WriteLine(n16_t5_2);
+
+            Console.WriteLine("T4(7, 0.96875)");
+            Console.WriteLine(n4_t4_3);
+            Console.WriteLine(n8_t4_3);
+            Console.WriteLine(n16_t4_3);
+            Console.WriteLine("T5(7, 0.96875)");
+            Console.WriteLine(n4_t5_3);
+            Console.WriteLine(n8_t5_3);
+            Console.WriteLine(n16_t5_3);
+
+            Console.WriteLine("T2(6.5, 0.4375)");
+            Console.WriteLine(n4_t2_4);
+            Console.WriteLine(n8_t2_4);
+            Console.WriteLine(n16_t2_4);
+            Console.WriteLine("T4(4.78125, 0.0625)");
+            Console.WriteLine(n4_t4_4);
+            Console.WriteLine(n8_t4_4);
+            Console.WriteLine(n16_t4_4);
+            Console.WriteLine("T5(4.78125, 0.0625)");
+            Console.WriteLine(n4_t5_4);
+            Console.WriteLine(n8_t5_4);
+            Console.WriteLine(n16_t5_4);
+
+            Console.WriteLine("T4(2, 0.5)");
+            Console.WriteLine(n4_t4_5);
+            Console.WriteLine(n8_t4_5);
+            Console.WriteLine(n16_t4_5);
+            Console.WriteLine("T5(2, 0.5)");
+            Console.WriteLine(n4_t5_5);
+            Console.WriteLine(n8_t5_5);
+            Console.WriteLine(n16_t5_5);
+
+            Console.WriteLine("T5(1, 0.9999975)");
+            Console.WriteLine(n4_t5_6);
+            Console.WriteLine(n8_t5_6);
+            Console.WriteLine(n16_t5_6);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# OwensT
+
+Generate Owen's T Function Expected Value
+
+## Definition
+
+![define](https://github.com/tk-yoshimura/OwensT/blob/main/figures/define.svg)  
+
+## Special Values
+
+![special values](https://github.com/tk-yoshimura/OwensT/blob/main/figures/svalue.svg)  
+
+## Properties
+
+![symmetry](https://github.com/tk-yoshimura/OwensT/blob/main/figures/symmetry.svg)  
+![formula](https://github.com/tk-yoshimura/OwensT/blob/main/figures/formula.svg)  
+
+## Expected Value Generation
+
+The Fast algorithm by Patefield and Tandy et al. is a method that uses six different algorithms depending on the region.
+However, at accuracies higher than double, the algorithms diverge in some cases and approximations cannot be obtained.
+Among the six algorithms, the Gaussian quadrature-based generation method is the most applicable to a wide range of domains and is stable without worrying about loss of digits.
+
+![t5algo](https://github.com/tk-yoshimura/OwensT/blob/main/figures/t5algo.svg)  
+[Gaussian quadrature Sampling Points](https://github.com/tk-yoshimura/LegendrePolynomialRoot)  
+
+When h is large, the Gaussian quadrature method may not be able to correctly calculate because integrand f takes a small value in the region where x is large and the sample points are concentrated.
+
+![gauss_integrand](https://github.com/tk-yoshimura/OwensT/blob/main/figures/gauss_integrand.svg)  
+![integrand_hp5](https://github.com/tk-yoshimura/OwensT/blob/main/figures/integrand_hp5.svg)  
+![integrand_h1](https://github.com/tk-yoshimura/OwensT/blob/main/figures/integrand_h1.svg)  
+![integrand_h2](https://github.com/tk-yoshimura/OwensT/blob/main/figures/integrand_h2.svg)  
+![integrand_h4](https://github.com/tk-yoshimura/OwensT/blob/main/figures/integrand_h4.svg)  
+![integrand_h8](https://github.com/tk-yoshimura/OwensT/blob/main/figures/integrand_h8.svg)  
+![integrand_h16](https://github.com/tk-yoshimura/OwensT/blob/main/figures/integrand_h16.svg)  
+
+## Reference
+
+Owen, D B. "Tables for computing bivariate normal probabilities". Annals of Mathematical Statistics, 27, 1075–1090. (1956)  
+Patefield, M. and Tandy, D. "Fast and accurate Calculation of Owen’s T-Function", Journal of Statistical Software, 5 (5), 1–25. (2000) 
